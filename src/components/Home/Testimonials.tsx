95b0ffda2582d3087258c587503433bb05bbeb89
blackheart7565 <give.me.heart8844@gmail.com>
2024-08-29 16:32:04 +0300

    feat: add responsiveness to Testimonials section

--- src/components/Home/Testimonials.tsx
+++ src/components/Home/Testimonials.tsx
@@ -9,23 +9,23 @@ interface ITestimonialsProps { }
 
 export const Testimonials: FC<ITestimonialsProps> = (): JSX.Element => {
 	return (
-		<section className="xlm:min-h-screen xl:h-screen relative">
-			<div className="flex flex-col items-center justify-center h-full py-28">
+		<section className="xlm:min-h-screen relative">
+			<div className="flex flex-col items-center justify-center h-full py-20">
 				<div className="container">
-					<h1 className="text-[65px] font-semibold tracking-[0.03em] dark:text-white text-black text-center">
+					<h1 className="text-4xl xl:text-[65px] font-semibold tracking-[0.03em] dark:text-white text-black text-center">
 						Testimonials
 					</h1>
-					<p className="text-[21px] tracking-[0.03em] dark:text-white text-black text-center max-w-4xl mx-auto mt-4">
+					<p className="text-base xl:text-[21px] tracking-[0.03em] dark:text-white text-black text-center max-w-4xl mx-auto mt-4">
 						Lorem ipsum dolor sit amet consectetur. Imperdiet convallis blandit felis ligula aliquam venenatis fghh hgjj nisi ante.
 					</p>
 				</div>
 				<Swiper
 					spaceBetween={70}
 					slidesPerView={1}
-					className="w-full h-max px-96 mt-20 testimonialsSwiper"
+					className="w-full h-max px-2 lg:px-40 xl:px-60 2xl:px-96 mt-20 testimonialsSwiper"
 				>
 					<SwiperSlide
-						className="max-h-[345px]"
+						className="xl:max-h-[345px]"
 					>
 						<TestimonialsCard
 							img={"/testimonials-img1.png"}
--- src/components/Home/TestimonialsCard.tsx
+++ src/components/Home/TestimonialsCard.tsx
@@ -14,16 +14,16 @@ export const TestimonialsCard: FC<ITestimonialsCardProps> = ({
 	ceo,
 }): JSX.Element => {
 	return (
-		<div className="flex items-center bg-white px-14 py-10 gap-10 w-full h-full rounded-xl cursor-pointer">
-			<div className="w-[235px] h-[235px] shrink-0">
+		<div className="flex flex-col sm:flex-row items-center bg-white px-14 py-10 gap-10 w-full h-full rounded-xl cursor-pointer">
+			<div className="w-[150px] h-[150px] lg:w-[235px] lg:h-[235px] shrink-0">
 				<img
 					src={img}
 					alt="testimonials-img1"
 					className="w-full h-full object-cover object-center"
 				/>
 			</div>
-			<div className="flex flex-col text-black">
-				<p className="text-[21px] tracking-[0.03em] text-balance">{description}</p>
+			<div className="flex flex-col text-black gap-1">
+				<p className="sm:text-lg xl:text-[21px] tracking-[0.03em] text-balance">{description}</p>
 				<span className="text-2xl font-medium tracking-[0.03em] ">{name}</span>
 				<span className="text-[19px] tracking-[0.03em]">{ceo}</span>
 			</div>
